65c721c2a6a23b62ecc1c77a8f8ab6a6e7f36f7d
Chris Downing <aka.the.phoss@gmail.com>
2019-02-24 09:06:34 +1000

    TG28 Refactoring/Renaming/Moving

--- Source/S5_TestingGrounds/S5_TestingGroundsGameMode.cpp
+++ Source/S5_TestingGrounds/S5_TestingGroundsGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "S5_TestingGroundsGameMode.h"
 #include "S5_TestingGroundsHUD.h"
-#include "S5_TestingGroundsCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AS5_TestingGroundsGameMode::AS5_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
